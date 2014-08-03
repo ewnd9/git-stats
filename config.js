b177eb6876b8715dc6fae956f58c8210fdf61d66
Your Name <ewndnine@gmail.com>
2014-08-03 20:29:17 +0900

    colors from github

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     filepath: process.env.HOME + '/.gitstats',
     html: process.env.HOME + '/.gitstats.html',
-    colors: ['#CCC', '#13E87D', '#22FF58', '#08E806'],
+    colors: ['#eee', '#d6e685', '#8cc665', '#44a340', '#1e6823'],
     format: 'MM.DD',
     commands: ['commit'],
     period: 90
