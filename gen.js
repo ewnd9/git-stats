75dbe33ca40b0a849d89fe8d9ad4801e773f278c
Your Name <ewndnine@gmail.com>
2015-01-06 18:43:06 +0900

    dump

--- gen.js
+++ gen.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var moment = require('moment');
 
 var config = require('./config');
+var jade = require('jade');
 
 var path = config.filepath;
 var htmlPath = config.html;
@@ -12,7 +13,6 @@ var period = config.period;
 fs.statSync(path);
 var data = JSON.parse(fs.readFileSync(path, 'utf-8'))['data'];
 
-var html = '<table><tr>';
 var commits = {};
 
 for (var i = 0 ; i < data.length ; i++) {
@@ -27,21 +27,35 @@ for (var i = 0 ; i < data.length ; i++) {
 }
 
 var date = moment().subtract(period, 'day');
+
+var currEvents = [];
+
+var events = [];
+events.push(curr);
+
 for (var i = 0 ; i < period ; i++) {
-    date.add(1, 'day');
+  date.add(1, 'day');
 
-    var curr = date.format(fmt);
-    var size = commits[curr] ? commits[curr].length : 0;
-    var color = config.colors[Math.min(size, config.colors.length - 1)];
+  var curr = date.format(fmt);
+  var size = commits[curr] ? commits[curr].length : 0;
+  var color = config.colors[Math.min(size, config.colors.length - 1)];
 
-    html += '<td ' + 'style="background-color: ' + color + '">' + date.format(config.format) +  '</td>';
+  currEvents.push({
+    color: color,
+    date: date.format(config.format)
+  });
 
-    if ((i + 1) % 5 == 0) {
-        html += '</tr>\n<tr>';
-    }
+  if ((i + 1) % 5 == 0) {
+    currEvents = [];
+    events.push(currEvents);
+  }
 }
 
-html += '</tr></table>';
+var swig  = require('swig');
+
+var html = swig.renderFile(__dirname + '/stats-template.html', {
+  events: events
+});
 
 fs.writeFile(htmlPath, html, "utf-8", function (err) {
   if (err) throw err;
@@ -49,5 +63,3 @@ fs.writeFile(htmlPath, html, "utf-8", function (err) {
 
 var open = require('open');
 open(htmlPath);
-
-console.log(html);
